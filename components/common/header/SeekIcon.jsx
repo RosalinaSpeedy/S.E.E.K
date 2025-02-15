a6458b61035708ffe9cbd1516f5dac6e23c387a0
RosalinaSpeedy <86687184+RosalinaSpeedy@users.noreply.github.com>
2025-02-15 22:31:44 +0000

    save all changes

--- components/common/header/SeekIcon.jsx
+++ components/common/header/SeekIcon.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
-import { TouchableOpacity, Image, View } from 'react-native'
+import { TouchableOpacity, Image } from 'react-native'
 
 import styles from './screenheader.style'
 
 const SeekIcon = ({ iconUrl, dimension, handlePress }) => {
   return (
-    <View>
-        <TouchableOpacity onPress={handlePress} style={styles.buttonContainer}>
-        <Image 
-            source={iconUrl}
-            resizeMode='cover'
-            style={styles.iconImage}
-        />
-        </TouchableOpacity>
-    </View>
+    <TouchableOpacity onPress={handlePress} style={styles.buttonContainer}>
+      <Image 
+        source={iconUrl}
+        resizeMode='cover'
+        style={styles.iconImage}
+      />
+    </TouchableOpacity>
   )
 }
 
--- components/quiz/TriggerInputs.jsx
+++ components/quiz/TriggerInputs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Text, TextInput, View } from 'react-native'
 
+
 import styles from './quiz.style'
 
 const TriggerInputs = () => {
