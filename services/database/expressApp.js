1c31b9455d14c4f7a783e71c321fc1360265ef95
RosalinaSpeedy <86687184+RosalinaSpeedy@users.noreply.github.com>
2025-03-27 13:54:36 +0000

    basic express app for forum database connectivity implemented - it now connects

---
+++ services/database/expressApp.js
@@ -0,0 +1,55 @@
+//Databases and the web code (ammended)
+
+// Import express and express-session
+var express = require('express')
+var session = require('express-session')
+var validator = require ('express-validator');
+const expressSanitizer = require('express-sanitizer');
+
+//Import mysql module
+var mysql = require('mysql2')
+
+
+// Create the express application object
+const app = express()
+const port = 8000
+
+// Set up the body parser 
+app.use(express.urlencoded({ extended: true }))
+
+// Create an input sanitizer
+app.use(expressSanitizer());
+
+// Define the database connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'seek_forum_app',
+    password: 'qwertyuiop',
+    database: 'seek_forum'
+})
+// Connect to the database
+db.connect((err) => {
+    if (err) {
+        throw err
+    }
+    console.log('Connected to database')
+})
+
+global.db = db
+
+// Create a session
+app.use(session({
+    secret: 'somerandomstuff',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}))
+
+// Load the route handlers for /api
+const apiRoutes = require('../routes/api')
+app.use('/api', apiRoutes)
+
+// Start the web app listening
+app.listen(port, () => console.log(`Node app listening on port ${port}!`))
