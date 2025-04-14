04ef6047f3a4ee1c48b03df295fa259cea76a176
RosalinaSpeedy <86687184+RosalinaSpeedy@users.noreply.github.com>
2025-04-14 22:06:38 +0100

    some AI stuff. none of it works

--- app/index.js
+++ app/index.js
@@ -69,3 +69,28 @@ const homeScreen = () => {
 }
 
 export default homeScreen;
+
+// import { useState } from 'react';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { torch } from 'react-native-pytorch-core';
+
+// export default function App() {
+//   const [tensor, _setTensor] = useState(torch.rand([2, 3]));
+//   return (
+//     <View style={styles.container}>
+//       <Text>{`Random tensor of shape ${tensor.shape} with data ${tensor.data()}`}</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
+
